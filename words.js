923ddb16337192219cd76a065b8eb6a5f7393978
aalborghetti <alberto.alborghetti@gmail.com>
2026-02-26 17:45:12 +0100

    Create words.js

---
+++ words.js
@@ -0,0 +1,21 @@
+// words.js
+// Lista parole + suggerimento "simile" per aiutare l'impostore.
+// Puoi aggiungere/rimuovere elementi liberamente.
+
+const WORD_PAIRS = [
+  { word: "Pizzeria", hint: "Ristorante" },
+  { word: "Aeroporto", hint: "Stazione" },
+  { word: "Spiaggia", hint: "Mare" },
+  { word: "Biblioteca", hint: "Libreria" },
+  { word: "Ospedale", hint: "Clinica" },
+  { word: "Stazione", hint: "Fermata" },
+  { word: "Museo", hint: "Galleria" },
+  { word: "Palestra", hint: "Fitness" },
+  { word: "Castello", hint: "Fortezza" },
+  { word: "Supermercato", hint: "Negozio" },
+  { word: "Cinema", hint: "Teatro" },
+  { word: "Scuola", hint: "Classe" },
+  { word: "Ristorante", hint: "Trattoria" },
+  { word: "Teatro", hint: "Spettacolo" },
+  { word: "Parco", hint: "Giardino" },
+];
